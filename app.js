375a5d612239412627dc2e699ab657a51c695d43
abhivarma <abhilash.dongari2@gmail.com>
2023-03-02 01:59:43 +0530

    Express-sessions part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -80,15 +82,28 @@ app.use(express.urlencoded({ extended: false }));
 //   }
   
 // }
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+
+
+
 
 function auth (req, res, next) {
+  console.log(req.session);
 
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
         var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
+        res.setHeader('WWW-Authenticate', 'Basic');                        
         err.status = 401;
         next(err);
         return;
@@ -97,25 +112,26 @@ function auth (req, res, next) {
     var user = auth[0];
     var pass = auth[1];
     if (user == 'admin' && pass == 'password') {
-        res.cookie('user','admin',{signed: true});
+        req.session.user = 'admin';
         next(); // authorized
     } else {
         var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
+        res.setHeader('WWW-Authenticate', 'Basic');
         err.status = 401;
         next(err);
     }
+}
+else {
+  if (req.session.user === 'admin') {
+      console.log('req.session: ',req.session);
+      next();
   }
   else {
-      if (req.signedCookies.user === 'admin') {
-          next();
-      }
-      else {
-          var err = new Error('You are not authenticated!');
-          err.status = 401;
-          next(err);
-      }
+      var err = new Error('You are not authenticated!');
+      err.status = 401;
+      next(err);
   }
+}
 }
  app.use(auth);
 //  ##***********************Cookies************************##
